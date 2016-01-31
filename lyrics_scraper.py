aa9aa7d1527b90744f68ca41e7cbc9663e80879b
Tyler McDonnell <tyler@cs.utexas.edu>
2016-01-31 02:21:34 -0600

    A little bit cleaner, I think.

--- lyrics_scraper.py
+++ lyrics_scraper.py
@@ -13,12 +13,7 @@ def fetch_lyrics(artist_name, song_name):
     return default_source(f_artist, f_song)   
 
 def _fetch_from_lyricsdotcom(artist, song):
-    # Format for URL structure.
-    artist = artist.replace(' ', '-')
-    song   = song.replace(' ', '-')
     page   = requests.get('http://www.lyrics.com/%s-lyrics-%s.html' % (song, artist))
-
-    # Extract and process lyrics from page.
     soup   = BeautifulSoup(page.text, 'lxml')
     lyrics = str(soup.findAll('div', {'id' : 'lyrics'}))
     lyrics = mlstripper.strip_tags(lyrics)
@@ -29,6 +24,7 @@ def _format_name(name):
     # Names should contain only lowercase alphanumeric characters and spaces.
     name = name.replace('-', ' ').lower()
     name = (re.sub(r'[^\w\- ]+', '', name))
+    name = name.replace(' ', '-')
     return name
 
 # Configure web source.
